24aa00e99a7f1cfcd4c1cfe774d5163aa51151f7
Francesco Turello <fturello@monaco.mc>
2021-11-04 10:23:12 +0100

    fin preparation initialisation du projet

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var models = require('./routes/index')
 
 var app = express();
+
+var connect = require('./models/connection');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
